a7b4272a8e5357b2c79354ef4ea164293799b5cd
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-01-23 15:47:57 +0100

    Define and annotate the following variables with the specified values:

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+'''
+define and annotate the following variables with the specified values:
+. a, an integer with a value of 1
+. pi, a float with a value of 3.14
+. i_understand_annotations, boolean with a value of True school,
+a string with a value of “Holberton”
+'''
 
-""" 
-func that define and annotate the following variables
-with the specified values
-"""
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = "Holberton"
+school: str = 'Holberton'
