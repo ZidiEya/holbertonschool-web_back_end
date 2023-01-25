939f40860304c2e2ca240eac251d4274d3c9ede6
ZidiEya <3805@holbertonschool.com>
2023-01-25 15:09:34 +0000

    Write a coroutine called async_generator that takes no arguments.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,6 @@ import random
 from typing import Generator
 
 
-
 async def async_generator() -> Generator[float, None, None]:
     """func that generates a random number"""
     for i in range(10):
