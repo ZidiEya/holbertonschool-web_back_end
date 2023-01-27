df4211c28aeb5be117f5983c6cc2599c7e690fa6
ZidiEya <3805@holbertonschool.com>
2023-01-27 14:40:40 +0000

    class BasicCache that inherits from BaseCaching and is a caching system:

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-
-'''
-Basic cache
-'''
+""" 0. Basic dictionary
+"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-Func that Inherits from BaseCaching and is a caching system
+    """ BaseCache defines:
+  - overwrite functions 'put' and 'get'
 """
 
     def put(self, key, item):
         """
-func assign item for key of cache_data
+Assign to the dictionary self.cache_data the item
+value for the key key
 """
-        if key is None or item is None:
-            return
+        if key and item:
             self.cache_data[key] = item
 
             def get(self, key):
                 """
-func return value of cache_data linked to key
+Return the value in self.cache_data linked to key
 """
-                if key is None:
-                    return
-                    return self.cache_data.get(key, None)
+                return self.cache_data.get(key)
