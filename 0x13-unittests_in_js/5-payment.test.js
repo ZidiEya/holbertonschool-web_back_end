8a77329dc5d4f97cdae75664f76725c6f2a9205b
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-04-06 23:44:05 +0200

    You should be able to run the test suite using npm test 5-payment.test.js

--- 0x13-unittests_in_js/5-payment.test.js
+++ 0x13-unittests_in_js/5-payment.test.js
@@ -1,28 +1,21 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
+const expect = require("chai").expect;
+const sinon = require("sinon");
 
-const sendPaymentRequestToApi = require('./5-payment');
+const sendPaymentRequestToApi = require("./3-payment.js");
 
-describe('sendPaymentRequestToApi', () => {
-  let spy;
+describe("sendPaymentRequestToApi function", () => {
+  let consoleSpy = null;
+  beforeEach(() => (consoleSpy = sinon.spy(console, "log")));
+  afterEach(() => consoleSpy.restore());
 
-  beforeEach(() => {
-    spy = sinon.spy(console, 'log');
-  });
-
-  afterEach(() => {
-    spy.restore();
-  });
-
-  it('should log correct output of calculateNumber(100, 20)', () => {
+  it("sendPaymentRequestToAPI(100, 20)", () => {
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnceWithExactly('The total is: 120'));
-    expect(spy.calledOnce).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith("The total is: 120")).to.be.true;
   });
-
-  it('should log correct output of calculateNumber(10, 10)', () => {
+  it("sendPaymentRequestToAPI(10, 10)", () => {
     sendPaymentRequestToApi(10, 10);
-    expect(spy.calledOnceWithExactly('The total is: 20'));
-    expect(spy.calledOnce).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith("The total is: 20")).to.be.true;
   });
 });
