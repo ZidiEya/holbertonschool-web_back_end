f004d0788c64354f3a1173a1f2573ef428ff0d50
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-04-06 18:29:53 +0200

    You can assume a and b are always number

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -1,55 +1,82 @@
-const assert = require("assert");
-const calculateNumber = require("./1-calcul.js");
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
 
-describe("calculateNumber type == SUM", () => {
-  it("checks the output", () => {
-    assert.strictEqual(calculateNumber("SUM", 1.1, 2.5), 4);
-    assert.strictEqual(calculateNumber("SUM", 0.0, 0), 0);
+describe('calculateNumber', function () {
+  describe('Integers', function () {
+    it('should return 4', function () {
+      assert.strictEqual(calculateNumber('SUM', 1, 7), 8);
+    });
   });
-  it("negative numbers", () => {
-    assert.strictEqual(calculateNumber("SUM", -1, 1), 0);
-    assert.strictEqual(calculateNumber("SUM", -1.5, 0), -1);
-  });
-  it("checks arguments", () => {
-    assert.strictEqual(isNaN(calculateNumber("SUM", 2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber(2.2, 2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber(2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
-  });
-});
 
-describe("calculateNumber type == SUBSTRACT", () => {
-  it("checks the output", () => {
-    assert.strictEqual(calculateNumber("SUBSTRACT", 3.1, 2.5), 0);
-    assert.strictEqual(calculateNumber("SUBSTRACT", 0.0, 5), -5);
+  describe('One float', function () {
+    it('should return 8', function () {
+      assert.strictEqual(calculateNumber('SUM', 1, 6.7), 8);
+    });
   });
-  it("negative numbers", () => {
-    assert.strictEqual(calculateNumber("SUBSTRACT", -1, 1), -2);
-    assert.strictEqual(calculateNumber("SUBSTRACT", -1.5, 0), -1);
+
+  describe('One float', function () {
+    it('should return 5', function () {
+      assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
+    });
   });
-  it("checks arguments", () => {
-    assert.strictEqual(isNaN(calculateNumber("SUBSTRACT", 2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber(2.2, 2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber(2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
+
+  describe('One float', function () {
+    it('should return 5', function () {
+      assert.strictEqual(calculateNumber('SUM', 1, 5.3), 6);
+    });
   });
-});
-describe("calculateNumber type == DIVIDE", () => {
-  it("check the output", () => {
-    assert.strictEqual(calculateNumber("DIVIDE", 2, 2.5), 0.6666666666666666);
-    assert.strictEqual(calculateNumber("DIVIDE", 0.0, 2), 0);
+
+  describe('Two floats', function () {
+    it('should return 5', function () {
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+    });
   });
-  it("check negative numbers", () => {
-    assert.strictEqual(calculateNumber("DIVIDE", -1, 1), -1);
+
+  describe('Subtract two integers', function () {
+    it('should return -1', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2, 3), -1);
+    });
   });
-  it("check second argument is 0", () => {
-    assert.strictEqual(calculateNumber("DIVIDE", 2.2, 0), "Error");
+
+  describe('Subtract one float', function () {
+    it('should return 0', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2.9, 3), 0);
+    });
   });
-  it("check arguments", () => {
-    assert.strictEqual(isNaN(calculateNumber("DIVIDE", 2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber("DIVIDE")), true);
-    assert.strictEqual(isNaN(calculateNumber(2.2, 2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber(2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
+
+  describe('Subtract two float', function () {
+    it('should return 4', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 6.6, 3.2), 4);
+    });
+  });
+
+  describe('Subtract two float', function () {
+    it('should return 0', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 4.2, 3.9), 0);
+    });
+  });
+
+  describe('Divide two floats', function () {
+    it('should return 1', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 4.2, 3.9), 1);
+    });
+  });
+
+  describe('Divide one float', function () {
+    it('should return 1.3', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 4.2, 3), 1.3333333333333333);
+    });
+  });
+
+  describe('Divide one integer, and a 0', function () {
+    it('should return "Error"', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 2.2, 0), 'Error');
+    });
+  });
+
+  describe('Divide two integers', function () {
+    it('should return 1', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 2, 2), 1);
+    });
   });
 });
