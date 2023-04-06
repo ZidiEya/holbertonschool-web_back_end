b52a3be5d7460bae8fd5b77bf22fa1e542f15d59
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-04-06 23:39:04 +0200

    Imagine that calling the function Utils.calculateNumber is actually calling an API or a very expensive method. You don’t necessarily want to do that on every test run

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,20 +1,19 @@
-const sinon = require("sinon");
-const { expect } = require('chai')
+const expect = require('chai').expect;
+const sinon = require('sinon');
 
-const sendPaymentRequestToApi = require('./3-payment');
-const Utils = require('./utils');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
 
-describe('sendPaymentRequestToApi', () => {
-  it('Test behavior of sendPaymentRequestToApi same that Utils.calculateNumber', () => {
-    const stubUtils = sinon.stub(Utils, 'calculateNumber').returns(10);
-    const spyConsole = sinon.spy(console, 'log');
-    const apiResponse = sendPaymentRequestToApi(100, 20);
+describe('sendPaymentRequestToApi function', () => {
+  const consoleSpy = sinon.spy(console, 'log');
 
-    expect(stubUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(spyConsole.calledOnceWithExactly('The total is: 10')).to.be.true;
-    expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(apiResponse);
-
-    stubUtils.restore();
-    spyConsole.restore();
+  it('validate the usage of the Utils function', () => {
+    const utilStub = sinon.stub(Utils, 'calculateNumber');
+    utilStub.withArgs('SUM', 100, 20).returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    utilStub.restore()
+    consoleSpy.restore();
   });
 });
