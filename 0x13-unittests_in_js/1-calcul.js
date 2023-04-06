0f186adde96ec0d4abda156d39d3163ec0f80d24
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-04-06 18:28:33 +0200

    Add a new argument named type at first argument of the function. type can be SUM, SUBTRACT, or DIVIDE (string)

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,34 +1,18 @@
-const SUM = "SUM";
-const SUBTRACT = "SUBTRACT";
-const DIVIDE = "DIVIDE";
+function calculateNumber(type, a, b) {
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
 
-const isNegZero = (n) => {
-  n = Number(n);
-  return n === 0 && 1 / n === -Infinity;
-};
-
-module.exports = function calculateNumber(type, a, b = 0) {
-  let aNum = Number(a);
-  let bNum = Number(b);
-
-  if (Number.isNaN(aNum) || Number.isNaN(bNum))
-    throw TypeError("Parameters must be numbers or able to coerce to number");
-
-  aNum = Math.round(aNum);
-  bNum = Math.round(bNum);
-
-  switch (type) {
-    case SUM:
-      return aNum + bNum;
-    case SUBTRACT:
-      return aNum - bNum;
-    case DIVIDE:
-      if (bNum === 0) return "ERROR";
-      const res = aNum / bNum;
-      return isNegZero(res) ? 0 : res;
-    default:
-      throw Error(
-        'Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".'
-      );
+  if (type === 'DIVIDE') {
+    if (roundB === 0) {
+      return 'Error';
+    }
+    return roundA / roundB;
   }
-};
+
+  if (type === 'SUBTRACT') {
+    return roundA - roundB;
+  }
+
+  return roundA + roundB;
+}
+module.exports = calculateNumber;
