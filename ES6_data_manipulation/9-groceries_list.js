a73e6186029b4792479a99e2836b06de45d88ccd
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-05-21 15:35:40 +0100

    Create 9-groceries_list.js

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+// 9-groceries_list.js
+
+export default function groceriesList() {
+  const items = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  return new Map(items);
+}
