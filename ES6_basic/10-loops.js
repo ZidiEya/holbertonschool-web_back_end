d0b192c25a8071913fa046e6fdb198041dcd7ecc
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-05-21 02:19:27 +0100

    Create 10-loops.js

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+// 10-loops.js
+
+export default function appendToEachArrayValue(array, appendString) {
+  let index = 0;
+  for (let value of array) {
+    array[index] = appendString + value;
+    index++;
+  }
+  return array;
+}
