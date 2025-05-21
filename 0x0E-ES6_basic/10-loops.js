f8e3260d600f20d0b0666cba2ae00fa2538acd2b
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-05-21 01:41:30 +0100

    Delete 0x0E-ES6_basic directory

--- 0x0E-ES6_basic/10-loops.js
+++
@@ -1,9 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const newArray = array;
-  for (const val of array) {
-    const idx = array.indexOf(val);
-    newArray[idx] = appendString + val;
-  }
-
-  return newArray;
-}
