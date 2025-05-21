a65bc3bb3a55ee7fc815d3300622c08df01cd91b
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-05-21 14:57:49 +0100

    Create 3-get_ids_sum.js

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+// 3-get_ids_sum.js
+
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.reduce((sum, student) => sum + student.id, 0);
+}
