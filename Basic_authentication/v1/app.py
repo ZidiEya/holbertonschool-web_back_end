b56a6a3a368766632fc66ee08da923e230f249db
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-05-27 21:11:22 +0100

    Delete Basic_authentication/v1 directory

--- Basic_authentication/v1/app.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from flask import jsonify
-
-@app.errorhandler(401)
-def unauthorized_error(error):
-    """ 401 Unauthorized error handler """
-    return jsonify({"error": "Unauthorized"}), 401
--- Basic_authentication/v1/views/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" Index view """
-from flask import jsonify, abort
-from api.v1.views import app_views
-
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized():
-    """ GET /api/v1/unauthorized triggers 401 error """
-    abort(401)
