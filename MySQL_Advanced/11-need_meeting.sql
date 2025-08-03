01deb49cdc34abe04c2919fa5c33c621e8631fe2
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-08-03 14:22:32 +0100

    Update 11-need_meeting.sql

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,14 +1,12 @@
--- Drop the table if it exists
+-- Initial
 DROP TABLE IF EXISTS students;
 
--- Create the students table
 CREATE TABLE IF NOT EXISTS students (
     name VARCHAR(255) NOT NULL,
-    score INT DEFAULT 0,
-    last_meeting DATE NULL
+    score INT default 0,
+    last_meeting DATE NULL 
 );
 
--- Insert student data
 INSERT INTO students (name, score) VALUES ("Bob", 80);
 INSERT INTO students (name, score) VALUES ("Sylvia", 120);
 INSERT INTO students (name, score) VALUES ("Jean", 60);
