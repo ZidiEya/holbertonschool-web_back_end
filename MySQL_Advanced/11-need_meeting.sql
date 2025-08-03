62e439bcf3c697a4a8d5d85dfed1f16140a62057
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-08-03 15:01:07 +0100

    Update 11-need_meeting.sql

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,15 +1,10 @@
 -- Initial
-DROP TABLE IF EXISTS students;
 
-CREATE TABLE IF NOT EXISTS students (
-    name VARCHAR(255) NOT NULL,
-    score INT default 0,
-    last_meeting DATE NULL 
-);
-
-INSERT INTO students (name, score) VALUES ("Bob", 80);
-INSERT INTO students (name, score) VALUES ("Sylvia", 120);
-INSERT INTO students (name, score) VALUES ("Jean", 60);
-INSERT INTO students (name, score) VALUES ("Steeve", 50);
-INSERT INTO students (name, score) VALUES ("Camilia", 80);
-INSERT INTO students (name, score) VALUES ("Alexa", 130);
+CREATE OR REPLACE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (
+    last_meeting IS NULL
+    OR last_meeting < CURDATE() - INTERVAL 1 MONTH
+  );
