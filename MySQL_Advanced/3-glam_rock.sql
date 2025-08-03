bb360e8f8ff5b3cf811518281dee5f84311597a1
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-08-03 13:49:14 +0100

    Update 3-glam_rock.sql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
--- Select glam rock bands and calculate their lifespan using formed and split years
-SELECT
-  band_name,
-  IFNULL(split, YEAR(CURDATE())) - formed AS lifespan
-FROM
-  metal_bands
-WHERE
-  style = 'Glam rock'
-ORDER BY
-  lifespan DESC;
+-- Search bands with style Glam rock
+-- Current timing
+
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
+FROM metal_bands 
+WHERE style LIKE '%Glam rock%';
