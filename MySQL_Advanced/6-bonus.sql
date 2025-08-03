5986b09eeecd487c362ffd67de9129e87ecc62f3
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-08-03 13:57:51 +0100

    Update 6-bonus.sql

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,28 +1,18 @@
-DELIMITER $$
-
+-- Stored procedure AddBonus that adds a new correction for a student.
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER //
 CREATE PROCEDURE AddBonus(
     IN user_id INT,
     IN project_name VARCHAR(255),
-    IN score INT
-)
+    IN score FLOAT)
 BEGIN
     DECLARE project_id INT;
-
-    -- Check if the project already exists
-    SELECT id INTO project_id
-    FROM projects
-    WHERE name = project_name
-    LIMIT 1;
-
-    -- If project doesn't exist, insert it
-    IF project_id IS NULL THEN
-        INSERT INTO projects(name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
     END IF;
-
-    -- Insert the correction
-    INSERT INTO corrections(user_id, project_id, score)
-    VALUES (user_id, project_id, score);
-END$$
-
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+END
+//
 DELIMITER ;
